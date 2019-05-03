1b0ece043fd66b779c57c31f0e647336b072dc80
Mark Barr <mark@hyperingenuity.com>
2019-05-03 19:29:15 -0400

    yarn: basic yarn module added

---
+++ modules/yarn.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function checkout(kraken, step) {
+  if (step.install !== false) {
+    kraken.exec('yarn install');
+  }
+
+  let command = `yarn ${ step.yarn }`;
+
+  if (step.args) {
+    command += ` ${ step.args }`;
+  }
+
+  const result = kraken.exec(command);
+
+  return result;
+}
+
+module.exports = checkout;
