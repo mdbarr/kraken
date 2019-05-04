7126678ba4e02540181828cafb2534b63716a6cf
Mark Barr <mark@hyperingenuity.com>
2019-05-03 20:07:40 -0400

    package: inital packager module for tar + gz, xz, bzip2

---
+++ modules/package.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function packager(kraken, step) {
+  let command = 'tar -v -c';
+
+  if (step.name.endsWith('gz')) {
+    command += ' --gzip';
+  } else if (step.name.endsWith('xz')) {
+    command += ' --xz';
+  } else if (step.name.endsWith('bz2')) {
+    command += ' --bzip2';
+  }
+
+  command += ` -f ${ step.name } ${ step.package }`;
+
+  const result = kraken.exec(command);
+
+  return result;
+}
+
+module.exports = packager;
