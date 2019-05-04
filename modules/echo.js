8d6e58ecccddebd9d6f07585a1cdd41b47749a60
Mark Barr <mark@hyperingenuity.com>
2019-05-04 15:49:26 -0400

    echo: simple echo module (console.log) pp: pretty print module (console.pp)

---
+++ modules/echo.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function echo(kraken, step) {
+  console.log(step.echo);
+}
+
+module.exports = echo;
---
+++ modules/pp.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function pp(kraken, step) {
+  console.pp(step.pp, step);
+}
+
+module.exports = pp;
